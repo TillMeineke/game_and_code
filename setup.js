da2f9656e12acb7cd93081d3250127c777a93b95
Till Meineke <87139424+TillMeineke@users.noreply.github.com>
2023-09-06 19:25:41 +0200

    Initial commit

---
+++ setup.js
@@ -0,0 +1,15 @@
+export const player = {
+  run1: "/assets/fw_1.png",
+  run2: "/assets/fw_2.png",
+  jumping: "/assets/fj.png",
+  lose: "/assets/fh.png",
+  rotate: true
+};
+
+// export const theme = "night"; //"bw" or "night"
+export const customColors = {
+  textColor: "midnightblue",
+  textBackgroundColor: "papayawhip",
+  skyColor: "lightsalmon",
+  gameBackgroundColor: "midnightblue"
+};
